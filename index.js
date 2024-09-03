a3813f17a04c7caf47877a3313a5e16a4f5a88c6
AOP Administrator <muhammad.najib@ag-it.com>
2024-09-03 21:36:01 +0700

    modify validation

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 let Octokit;
 
+// Load dependencies dynamically
 async function loadDependencies() {
   if (!Octokit) {
     const octokitModule = await import("@octokit/rest");
-    Octokit = octokitModule.Octokit;
+    Octokit = octokitModule.Octokit; // Ensure Octokit is globally available
   }
 }
 
+// Fetch team members from a GitHub organization team
 async function getTeamMembers(octokit, org, team_slug) {
   try {
     const { data: teamMembers } = await octokit.teams.listMembersInOrg({
@@ -16,27 +18,31 @@ async function getTeamMembers(octokit, org, team_slug) {
       team_slug,
     });
     return teamMembers.map(member => member.login);
-  } catch (error) {
-    core.setFailed(`Failed to fetch team members for ${team_slug}: ${error.message}`);
-    return [];
+  } catch (err) {
+    core.setFailed(`Failed to fetch team members for ${team_slug}: ${err.message}`);
+    return []; // Return an empty array on failure
   }
 }
 
-async function getCodeOwnersContent(octokit, owner, repo) {
+// Retrieve CODEOWNERS content from a repository
+async function getCodeOwnersContent(octokit, owner, repo, codeOwnerPath) {
   try {
     const { data } = await octokit.repos.getContent({
       owner,
       repo,
-      path: '.github/CODEOWNERS',
+      path: codeOwnerPath,
     });
     const content = Buffer.from(data.content, 'base64').toString('utf-8');
-    return content;
-  } catch (error) {
-    core.setFailed(`Failed to fetch CODEOWNERS file: ${error.message}`);
-    return null;
+    if (content.trim() !== '') {
+      return content; // Return the content as soon as it's found
+    }
+  } catch (err) {
+    core.setFailed('CODEOWNERS file not found.');
+    return null; // Return null if not found after checking all locations
   }
 }
 
+// List code owner approvals for a pull request
 async function listCodeOwnerApprovals(octokit, owner, repo, pull_number, codeOwnersContent) {
   const { data: reviews } = await octokit.pulls.listReviews({
     owner,
@@ -44,49 +50,65 @@ async function listCodeOwnerApprovals(octokit, owner, repo, pull_number, codeOwn
     pull_number,
   });
 
+  // Process CODEOWNERS file content (team or individual users)
   const lines = codeOwnersContent.split('\n').filter(line => !line.startsWith('#') && line.trim() !== '');
   let codeOwners = [];
   for (const line of lines) {
     const parts = line.split(/\s+/).slice(1);
     for (const part of parts) {
-      if (part.startsWith('@') && part.includes('/')) { // Indicates a team
+      if (part.startsWith('@') && part.includes('/')) { // team
         const [org, team_slug] = part.slice(1).split('/');
         const teamMembers = await getTeamMembers(octokit, org, team_slug);
         codeOwners = codeOwners.concat(teamMembers);
       } else {
-        codeOwners.push(part.replace('@', ''));
+        codeOwners.push(part.replace('@', '')); // user
       }
     }
   }
 
+  // Filter approved reviews and match with code owners
   const approvedUsers = reviews.filter(review => review.state === 'APPROVED').map(review => review.user.login);
   const matchedApprovals = approvedUsers.filter(username => codeOwners.includes(username)).length;
 
+  // Log approval details
   console.log("=====================================");
   console.log("Code owners list : ", codeOwners);
   console.log("Approved given by : ", approvedUsers);
   console.log("Matched approval : ", matchedApprovals);
   console.log("=====================================");
 
-  return matchedApprovals;
+  return matchedApprovals; // Return the count of matched approvals
 }
 
 async function main() {
   try {
     await loadDependencies();
     const this_octokit = new Octokit({ auth: process.env.AOSB2C_TOKEN });
+    const this_codeOwnerPath = core.getInput('codeowners-path');
+    const this_requiredApprovals = core.getInput('required-approvals');
+
+    if (!this_codeOwnerPath) {
+      throw new Error("CODEOWNERS file path not provided.");
+    }
+    console.log("CODEOWNERS file path : ", this_codeOwnerPath);
+
+
+    if (!this_requiredApprovals) {
+      throw new Error("Required approvals count not provided.");
+    }
+    console.log("Required approvals count : ", this_requiredApprovals);
 
     const { owner, repo } = github.context.repo;
     const pull_number = github.context.issue.number;
 
-    // Assuming getCodeOwnersContent now fetches team members
-    const teamMembers = await getCodeOwnersContent(this_octokit, owner, repo);
-    if (!teamMembers) return;
+    // Fetch CODEOWNERS content using the custom path
+    const codeOwnersContent = await getCodeOwnersContent(this_octokit, owner, repo, this_codeOwnerPath);
+    if (!codeOwnersContent) return; // Exit if CODEOWNERS content not found
 
-    // Assuming listCodeOwnerApprovals now checks for approvals from team members
-    const approvals = await listCodeOwnerApprovals(this_octokit, owner, repo, pull_number, teamMembers);
+    // Check for approvals from code owners
+    const approvals = await listCodeOwnerApprovals(this_octokit, owner, repo, pull_number, codeOwnersContent);
 
-    if (approvals < 2) {
+    if (approvals < this_requiredApprovals) {
     core.setFailed("Not enough approvals from code owner team.");
     } else {
     console.log("Sufficient approvals from code owner team.");
